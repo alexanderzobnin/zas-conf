d9dcc40402f918083abb731945c158880feefcb2
Alexander Zobnin <alexanderzobnin@gmail.com>
2016-01-04 23:29:43 +0300

    Added 2 agents for backend

--- start_agents.sh
+++ start_agents.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 nohup zas_agent.py --start --listen 127.0.0.1 --port 10061 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas01.pid' &
 nohup zas_agent.py --start --listen 127.0.0.1 --port 10062 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas02.pid' &
+nohup zas_agent.py --start --listen 127.0.0.1 --port 10063 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas03.pid' &
+nohup zas_agent.py --start --listen 127.0.0.1 --port 10064 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas04.pid' &
 netstat -tln4 | grep :100[6-7][0-9]
 echo 'ZAS agents started'
