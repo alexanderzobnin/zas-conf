450211a8615cbb82bfa410765ab6be9272d91e95
Alexander Zobnin <alexanderzobnin@gmail.com>
2016-01-04 21:17:32 +0300

    Initial commit.

---
+++ start_agents.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+nohup zas_agent.py --start --listen 127.0.0.1 --port 10061 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas01.pid' &
+nohup zas_agent.py --start --listen 127.0.0.1 --port 10062 --redis_host 127.0.0.1 --redis_port 6379 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas02.pid' &
