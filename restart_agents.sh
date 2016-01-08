4910e78518e7629d9978f8c7619f5887142d0798
Alexander Zobnin <alexanderzobnin@gmail.com>
2016-01-08 14:41:53 +0300

    Restart all redis instances

--- restart_agents.sh
+++ restart_agents.sh
@@ -3,7 +3,12 @@
 
 # Restart redis instances
 echo 'Restarting redis instances'
-systemctl restart redis.service
-systemctl restart redis-6380.service
+systemctl stop redis.service
+systemctl stop redis-6380.service
+systemctl stop redis-6381.service
+
+systemctl start redis.service
+systemctl start redis-6380.service
+systemctl start redis-6381.service
 
 ./start_agents.sh
