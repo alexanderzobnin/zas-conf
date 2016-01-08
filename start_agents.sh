d5ab767c7103b0fd50f99c4dbe5020d5fb8eb810
Alexander Zobnin <alexanderzobnin@gmail.com>
2016-01-08 14:31:11 +0300

    Fixed starting frontend agents

--- start_agents.sh
+++ start_agents.sh
@@ -8,10 +8,10 @@ zas_agent.py --start --listen 127.0.0.1 --port 10064 --redis_host 127.0.0.1 --re
 echo 'Backend group started'
 
 # Start frontend group
-zas_agent.py --start --listen 127.0.0.1 --port 10071 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas11.pid' --daemonize
-zas_agent.py --start --listen 127.0.0.1 --port 10072 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas12.pid' --daemonize
-zas_agent.py --start --listen 127.0.0.1 --port 10073 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas13.pid' --daemonize
-zas_agent.py --start --listen 127.0.0.1 --port 10074 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_backend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas14.pid' --daemonize
+zas_agent.py --start --listen 127.0.0.1 --port 10071 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_frontend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas11.pid' --daemonize
+zas_agent.py --start --listen 127.0.0.1 --port 10072 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_frontend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas12.pid' --daemonize
+zas_agent.py --start --listen 127.0.0.1 --port 10073 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_frontend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas13.pid' --daemonize
+zas_agent.py --start --listen 127.0.0.1 --port 10074 --redis_host 127.0.0.1 --redis_port 6380 --scenario '/usr/share/zas-conf/zas_scenario_frontend.cfg' --log '/var/log/zabbix/zas_agent.log' --user 'zabbix' --pid '/tmp/zas14.pid' --daemonize
 echo 'Frontend group started'
 
 netstat -tln4 | grep :100[6-8][0-9]
