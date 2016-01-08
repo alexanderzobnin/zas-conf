5a7f5288335f70024e7fe482a62b1ee53bf1d7f8
Alexander Zobnin <alexanderzobnin@gmail.com>
2016-01-08 17:26:28 +0300

    Added log info for redis start

--- restart_agents.sh
+++ restart_agents.sh
@@ -11,4 +11,9 @@ systemctl start redis.service
 systemctl start redis-6380.service
 systemctl start redis-6381.service
 
+# Show log info
+tail -n 1 /var/log/redis/redis.log
+tail -n 1 /var/log/redis/redis-6380.log
+tail -n 1 /var/log/redis/redis-6381.log
+
 ./start_agents.sh
